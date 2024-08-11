858d134deebd90a0b5af0b6d34522a9c2816d6c3
poemota <142114334+poeMota@users.noreply.github.com>
2024-08-11 22:08:19 +0300

    cleanup

--- src/token.py
+++ src/token.py
@@ -1,6 +1,3 @@
-from enum import Enum
-
-
 class Token:
     def __init__(self, key: any, value: any, indent: int, onList: bool = False) -> None:
         self.key = key
@@ -8,10 +5,6 @@ class Token:
         self.indent = indent
         self.childrens: list[Token] = []
         self.onList = onList
-
-
-    def __repr__(self) -> str:
-        return f"\nToken: \"{self.key}\", value: {self.value}, indent: {self.indent}, onList: {self.onList}, childrens ({len(self.childrens)}): {self.childrens}\n"
 
 
     def add_child(self, child: 'Token'):
